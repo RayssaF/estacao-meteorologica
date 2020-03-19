7702d5a214cd4d2367700fd21248528710609a4d
MatheusAnciloto <ancilotom@gmail.com>
2020-03-18 23:16:19 -0400

    Novas tabelas

--- Server/database/migrations/20200315015748-create-proprietario.js
+++ Server/database/migrations/20200315015748-create-proprietario.js
@@ -2,22 +2,21 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+   return queryInterface.createTable('Proprietario',{
+    id:{
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    nome:{
+      allowNull: false,
+      type: Sequelize.STRING,
+    },
+   });
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    queryInterface => queryInterface.dropTable('Proprietario');
   }
 };
--- Server/database/migrations/20200315172703-create-controlador.js
+++ Server/database/migrations/20200315172703-create-controlador.js
@@ -2,22 +2,25 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.createTable('Controlador',{
+      id:{
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      idModelo:{
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references:{
+          model: 'Modelo',
+          key: 'id',
+        }
+      }
+    });
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    queryInterface => queryInterface.dropTable('Controlador');
   }
 };
