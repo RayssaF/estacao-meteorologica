16ee7d0db6ac39abab55d307806b037cedb799c6
Willian Santos <desenvolvimento@grupoib3.com.br>
2020-04-11 16:48:35 -0400

    Fix Cors

--- Server/bin/server.js
+++ Server/bin/server.js
@@ -10,7 +10,13 @@ const { errors } = require('celebrate');
 const port = normalizePort(process.env.PORT || 4001);
 app.set('port', port);
 app.use(errors());
-app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 const server = http.createServer(app);
 server.listen(port);
 server.on('error', onError);
