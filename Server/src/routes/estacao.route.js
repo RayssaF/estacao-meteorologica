a72b7db9217890a2f0c46204bebd68d28a42a9f2
Willian Santos <desenvolvimento@grupoib3.com.br>
2020-04-11 09:57:30 -0400

    Adicionando Validation

--- Server/src/routes/estacao.route.js
+++ Server/src/routes/estacao.route.js
@@ -2,11 +2,29 @@ const express = require('express');
 const router = express.Router();
 const estacaoController = require('../controllers/estacao.controller');
 const eventoController = require('../controllers/evento.controller')
+const { celebrate, Joi, Segments } = require('celebrate');
 
 router.get('/',estacaoController.getAllLastEvents);
 router.get('/estacao',estacaoController.getAll);
-router.get('/estacao/:id',estacaoController.getAllById);
-router.get('/estacao/:id/eventos',eventoController.getAllByEstacao);
+router.get('/estacao/:id',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.number().required(),
+        }
+    })
+,estacaoController.getAllById);
+
+router.get('/estacao/:id/eventos',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.number().required(),
+        },
+        [Segments.QUERY]: {
+            page: Joi.number().required(),
+            pageSize: Joi.number().required()
+        }
+    }),
+    eventoController.getAllByEstacao);
 
 
 module.exports = router;
