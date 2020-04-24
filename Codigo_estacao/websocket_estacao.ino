ab006ef4929cdfcd87e06b9001f6383c280aef5b
victorbailiot <victor_bailiot@hotmail.com>
2020-04-24 11:23:13 -0400

    Alteração no código para WH2081
    Alteração nos dados de envio do websocket

--- Codigo_estacao/websocket_estacao.ino
+++ Codigo_estacao/websocket_estacao.ino
@@ -1,38 +1,28 @@
-//biblioteca DHT11
-#include <dht.h>
-//biblioteca Shield
-#include <SPI.h>
 #include <Ethernet.h>
-//Biblioteca Websocket
 #include <ArduinoHttpClient.h>
 //Biblioteca Estação
 #include "WeatherSensor.h"
 #define RF_IN  2
 
-//====DHT Sensor=====
-int    temperatura = 0x00, umidade     = 0x00;
-dht my_dht;
-//===================
 byte i;
 byte *packet;
 String guId = "3fb944";
+int messageSize = 0;
 
 // Definição dos Leds;
-#define dhtsensor 11
-#define dht_pin 12
+#define ledon 11
+#define ledstats 12
 
 // Instanciamento do serviço de rede com atribuição de um nome
 // para o servidor(server) e da porta(80) utilizada;
-char serverAddress[] = "meteorologia.adsvilhena.ninja/api";  // Endereço do servidor socket
+char serverAddress[] = "cryptic-cove-06248.herokuapp.com";  // Endereço do servidor socket
 int port = 80;
-byte mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02};
+byte mac[] = {0x00, 0x01, 0xBB, 0x12, 0xDE, 0x02};
 byte ip[] = { 192, 168, 0, 15 };
 
 EthernetClient cliente;
 WebSocketClient client = WebSocketClient(cliente, serverAddress, port);
 int count = 0;
-
-//========================================BOOT ESTAÇÃO=======================================================================================
 
 WeatherSensor weather;
 
@@ -70,14 +60,18 @@ void setup() {
   Serial.println("Iniciado!");
   Serial.println("Aguardando dados da estacao...");
   pinMode(RF_IN, INPUT);
-  pinMode(dhtsensor, OUTPUT);
-  sei(); //<-------enable interrupts--------
+  pinMode(ledon, OUTPUT);
+  pinMode(ledstats, OUTPUT);
+  sei();
   Serial.print("IP Address: ");
   Serial.println(Ethernet.localIP());
-
-} //  END-SETUP
+}
 
 void loop() {
+      if (client.available()) {
+      char c = client.read();
+      Serial.print(c);
+      }
       client.begin();
       delay(500);
       while (client.connected()) {
@@ -88,27 +82,23 @@ void loop() {
           {
             
             String dadosws = guId + "|" + weather.get_temperature() + "|" + weather.get_humidity() + "|" + 
-            weather.get_rainfall() + "|" + weather.get_wind_gust_kmh() + "|" + weather.get_wind_direction_str();
-            
-            delay(500);
-            
+            weather.get_rainfall() + "|" + weather.get_wind_gust_kmh() + "|" /*+ weather.get_wind_direction_str()*/;
+        
             //Montagem e envio da string com dados
             client.beginMessage(TYPE_TEXT);
             client.print(dadosws);
             client.endMessage();
-            Serial.println("Mensagem enviada !");
-            //Auto incremento do contador para próxima mensagem
-            count++;
-            //verificação se alguma mensagem está disponível para ser recebida
-            int messageSize = client.parseMessage();
+            messageSize = client.parseMessage();
             if (messageSize > 0) {
               Serial.println("Retorno recebido");
               Serial.println(client.readString());
             }
+            messageSize = 0;
             // Delay de 5 segundos
-            delay(5000);
+            delay(20000);
           }
           weather._interval = 0;
         }
     }
+    client.stop();
 }
